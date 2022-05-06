e28343866f3695b5fc05ef55be6915439f37d453
bree parker <breannaparker1991@gmail.com>
2022-05-06 01:23:08 -0600

    continued

---
+++ flask_app/controllers/trips.py
@@ -0,0 +1,41 @@
+from flask_app import app
+from flask import redirect, request, render_template, session, flash
+from flask_app.models.user import User
+from flask_app.models.list import List
+from flask_app.models.trip import Trip
+from flask_app.models.item import Item
+
+
+app.route('/trip')
+def new():
+  if 'user_id' not in session:
+    return redirect('/logout')
+  data = {
+    'id': session['user_id']
+  }
+  return render_template("trip.html", user = User.get_one(data))
+  
+@app.route('/new/trip', methods= ['POST'])
+def create():
+  if 'user_id' not in session:
+    return redirect ('/logout')
+  if not List.validate(request.form):
+    print('oops, theres a problem')
+    return redirect('/new')
+  print(request.form)
+  data = {
+    'name': request.form['name'],
+    'description': request.form['description'],
+  }
+  Trip.save(data)
+  return redirect('/welcome')
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+  if 'user_id' not in session:
+    return redirect('/logout')
+  data = {
+    'id' : id
+  }
+  Trip.destroy(data)
+  return redirect('/welcome')
