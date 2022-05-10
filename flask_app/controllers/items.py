55364dabee7f382b1d9d4ea7dfc79da32b8c5b18
bree parker <breannaparker1991@gmail.com>
2022-05-10 11:41:10 -0600

    quereis

--- flask_app/controllers/items.py
+++ flask_app/controllers/items.py
@@ -5,49 +5,6 @@ from flask_app.models.list import List
 from flask_app.models.trip import Trip
 from flask_app.models.item import Item
 
-@app.route('/create/list', methods= ['POST'])
-def create():
-  if 'user_id' not in session:
-    return redirect ('/logout')
-  if not List.validate(request.form):
-    print('oops, theres a problem')
-    return redirect('/new')
-  print(request.form)
-  data = {
-    'name': request.form['name'],
-    'description': request.form['description'],
-    'user_id': session['user_id'],
-  }
-  list = List.save(data)
-  print(list)
-  return redirect(f'/add/items/{list}')
-
-@app.route('/add/items/<int:id>')
-def add_items(id):
-  if 'user_id' not in session:
-    return redirect ('/logout')
-  data = {
-    'id': id
-  }
-  user = {
-    'id': session['user_id'], 
-  }
-  list = List.get_one(data)
-  user = User.get_one(user)
-  return render_template('items.html', list = list, user = user)
-
-@app.route('/more/items', methods=['POST'])
-def more():
-  if 'user_id' not in session:
-    return redirect ('/logout')
-  print(request.form)
-  data = {
-    'name': request.form['name'],
-    # 'id': session['list_id']
-  }
-  Item.save(data)
-  return redirect('/welcome')
-
 @app.route('/destroy/item/<int:id>')
 def destroy_items(id):
   if 'user_id' not in session:
--- flask_app/models/item.py
+++ flask_app/models/item.py
@@ -41,6 +41,21 @@ class Item:
   def update(cls,data):
     query = "UPDATE item SET name=%(name)s, updated_at = NOW() WHERE id = %(id)s;"
     return connectToMySQL(cls.db).query_db(query,data)
+  
+  @classmethod
+  def get_all(cls):
+    query = "SELECT * FROM item JOIN list on list.id = item.list_id WHERE list.id = %(id)s; "
+    results = connectToMySQL(cls.db).query_db(query)
+    items = cls(results[0])
+    for r in results:
+      data = {
+        'id': r['list.id'],
+        'name': r['name'],
+        'created_at': r['created_at'],
+        'updated_at': r['updated_at'],
+      }
+      items.item.append(Item(data))
+    return items
 
   @staticmethod
   def validate(item):
