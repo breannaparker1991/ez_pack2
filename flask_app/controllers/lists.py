eb51e767b47d6298cd326e938f8c62781c01c233
bree parker <breannaparker1991@gmail.com>
2022-05-10 11:16:16 -0600

    save errors

--- flask_app/controllers/lists.py
+++ flask_app/controllers/lists.py
@@ -13,23 +13,6 @@ def new():
     'id': session['user_id']
   }
   return render_template("create.html", user = User.get_one(data))
-  
-@app.route('/create/list', methods= ['POST'])
-def create():
-  if 'user_id' not in session:
-    return redirect ('/logout')
-  if not List.validate(request.form):
-    print('oops, theres a problem')
-    return redirect('/new')
-  print(request.form)
-  data = {
-    'name': request.form['name'],
-    'description': request.form['description'],
-    'user_id': session['user_id'],
-  }
-  list = List.save(data)
-  print(list)
-  return redirect(f'/add/items/{list}')
 
 @app.route('/destroy/list/<int:id>')
 def destroy_list(id):
